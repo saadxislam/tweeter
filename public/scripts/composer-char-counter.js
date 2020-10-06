15053c8e19e6d7343984a14d64238c5698ac1ce2
Saad Behlul Islam <muhammadsaad1010@gmail.com>
2020-10-06 22:29:18 +0000

    added a dynamic counter, traveresed up the tree and backdown to manipulate an elem, and gave a red color for a neg counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#tweet-text').keyup(function(evt){
+    const $input = $(this); //this is umm this
+    const inputDynamic = ($input.val().length); // input.val is a string, and .length gives us the dynamic value of our input
+    const form = $input.closest('form') //up the tree
+    const counter = form.find('.counter') // down the tree
+    counter.html(140 - inputDynamic); //counter.html accesses the innerHTML
+
+    if(inputDynamic > 140){
+      counter.addClass('redColor');
+    } else {
+      counter.removeClass('redColor');
+    }
+  });
+});
